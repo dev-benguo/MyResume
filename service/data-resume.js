da5e5fb57baeb7300139b38886d421845d39df9a
ben <ben.guo@codecanteen.com>
2018-08-18 00:15:33 +0800

    content update

--- service/data-resume.js
+++ service/data-resume.js
@@ -10,7 +10,7 @@ module.exports = [
         {
             title: "西安华杰网络科技有限公司",
             dateTime: "2014-05——2016-11",
-            desc: "本人负责公司项目的开发与维护工作。参与的项目有：ConnectCV、云雀窝、环球通项目。"
+            desc: "本人负责公司项目的开发与维护工作。参与的项目有：ConnectCV、云雀窝"
         },
         {
             title: "西安长城数字软件有限公司",
@@ -25,30 +25,30 @@ module.exports = [
         {
             title: "HSBC Mobile Banking",
             dateTime: "",
-            desc: "HSBC Mobile Banking是汇丰银行一款在线银行App，主要用于在线转账、支付、查询等功能。<br/>" + 
+            desc: "HSBC Mobile Banking是汇丰银行一款在线银行App，主要用于在线转账，支付，转账记录，汇率查询，信用卡借记卡激活、PIN码修改、停用等功能    。<br/>" + 
                   "责任描述：本人主要负责账户信息以及转账、支付功能的开发与维护工作。<br/>" +
-                  "项目中用到的软件和技术有：编程软件：VS Code，版本控制工具：Github、Git，自动化部署工具：Jenkins，web前端技术：Javascript、Html5、Css3、Jquery、RequireJS、Mustache"
+                  "项目中用到的软件和技术有: 编程软件VS code, 版本控制工具: Github、 Git, 自动化构建持续集成工具: Maven、Jenkins, 前端技术: JavaScript、 Html5、Css3、Jquery、Requirejs、Mustache等。"
         },
         {
-            title: "ConnectCV",
+            title: "Tabpear",
             dateTime: "",
-            desc: "ConnectCV是澳洲大学生在线求职招聘的网站。<br/>在线网站: <a target='_blank' href='http://www.connectcv.com'>connectcv.com</a>、<a target='_blank' href='http://blog.connectcv.com/'>blog.connectcv.com</a>。<br/> "+ 
-                  "责任描述：本人主要负责主页、简历等模块的前后端开发与维护工作。<br/>" +
-                  "项目中用到的软件和技术有：编程软件：MyEclipse，项目管理工具：Maven，数据库：Mysql，版本控制工具：Git，应用服务器：Tomcat、Wamp, web前端技术：Jsp、Javascript、Html5、Css3、Jquery，web框架技术：SpringMVC、Hibernate"
+            desc: "Tabpear是一款海内外营销运营以及提供软件服务的网站。<br/>在线网站: <a target='_blank' href='http://www.tabpear.com'>tabpear.com</a>" + 
+                  "责任描述：本人负责Tabpear网站前端框架搭建和开发工作。<br/>" + 
+                  "项目中用到的软件以及技术有：编程软件：Intellij IDEA、Visual Studio Code，项目管理工具：Maven，数据库：Mysql，版本控制工具：Git，应用服务器：Nginx, Web容器 Tomcat，web前端技术：Jsp、Javascript、Html5、Css3、Less、Jquery, Bootstrap, Vue，web框架技术：SpringMVC、JPA。"
         },
         {
             title: "云雀窝",
             dateTime: "",
             desc: "云雀窝是一个以社区装修服务为主的社交平台。<br/>" + 
                   "责任描述：本人主要负责云雀窝项目前端框架设计以及开发工作。<br/>" + 
-                  "项目中用到的软件以及技术有：编程软件：MyEclipse Sublime，项目管理工具：Maven，数据库：Mysql，版本控制工具：Git，应用服务器：Tomcat、Nginx，web前端技术：Jsp、Javascript、Html5、Css3、Less、Jquery、Grunt，web框架技术：SpringMVC、Hibernate。"
+                  "项目中用到的软件以及技术有：编程软件：MyEclipse Sublime，项目管理工具：Maven，数据库：Mysql，版本控制工具：Git，应用服务器：Nginx, Web容器: Tomcat，web前端技术：Jsp、Javascript、Html5、Css3、Less、Jquery、Bootstrap、Gulp，web框架技术：SpringMVC、Hibernate。"
         },
         {
-            title: "环球通项目",
+            title: "ConnectCV",
             dateTime: "",
-            desc: "环球通项目是针对建筑行业的解决方案包括以下子项目：开发者中心平台、基础官网、信息门户平台、总管理员平台。<br/>" + 
-                  "责任描述：本人主要负责各个项目前端框架设计以及开发工作。<br/>" +
-                  "项目中用到的软件以及技术有：编程软件：MyEclipse、Sublime，项目管理工具：Maven，数据库：Mysql，版本控制工具：Git，应用服务器：Tomcat、Nginx，web前端技术：Jsp、Javascript、Html5、Css3、Less、Jquery、Bootstrap、Gulp， web框架技术：SpringMVC、Mybatis。"
+            desc: "ConnectCV 是一款针对澳大利亚在校生的求职招聘网站。<br/>在线网站: <a target='_blank' href='http://www.connectcv.com'>connectcv.com</a>、<a target='_blank' href='http://blog.connectcv.com/'>blog.connectcv.com</a>。<br/> "+ 
+                  "责任描述：本人参与的模块有网站首页，学生账户 ConnectCV、 Portfolio、Career Assert 模块的开发， 以及雇佣者与教师账户中各模块UI修改以及功能优化和bug修改等。<br/>" +
+                  "项目中用到的软件和技术有：编程软件：MyEclipse，项目管理工具：Maven，数据库：Mysql，版本控制工具：Git，应用服务器：Apache, Web容器: Tomcat, web前端技术：Jsp、Javascript、Html5、Css3、Jquery，Jquery UI, web框架技术：SpringMVC、Hibernate。"
         }
     ]
 },
